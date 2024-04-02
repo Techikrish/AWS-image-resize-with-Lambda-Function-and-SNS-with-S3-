4cceaeee244f4b90e29961b96366063a036e6c96
Krishna Fattepurkar <krishnafattepurkar@gmail.com>
2024-04-02 15:45:44 +0530

    Added two script
    the image-resize-lambda-function.py is only lambda function script which triggers when image is uploaded into source bucket  
    the image-resize-with-SNS&Lambda.py is script which resize image with SNS notification

---
+++ image-resize-lambda-function.py
@@ -0,0 +1,51 @@
+import json
+import boto3
+from PIL import Image
+from io import BytesIO
+
+# Set your desired dimensions here
+width, height = 800, 600
+source_bucket_name = 'bucket1'  # Replace with your actual source bucket name
+destination_bucket_name = 'bucket2'  # Replace with your actual destination bucket name
+
+s3_client = boto3.client('s3')
+
+def resize_image(image_data):
+    image = Image.open(BytesIO(image_data))
+    resized_image = image.resize((width // 2, height // 2))  # Adjust dimensions as needed
+    return resized_image
+
+def lambda_handler(event, context):
+    try:
+        bucket_name = event['Records'][0]['s3']['bucket']['name']
+        object_key = event['Records'][0]['s3']['object']['key']
+
+        if not object_key.startswith("resized_"):
+            s3_response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+            image_data = s3_response['Body'].read()
+
+            resized_image = resize_image(image_data)
+
+            output_buffer = BytesIO()
+            resized_image.save(output_buffer, format='JPEG')  # Change format if necessary
+
+            output_buffer.seek(0)
+            new_object_key = f"resized_{object_key}"
+            s3_client.put_object(Bucket=destination_bucket_name, Key=new_object_key, Body=output_buffer)
+
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Image resizing complete!')
+            }
+        else:
+            print(f"Object {object_key} is already resized. Skipping processing.")
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Image already resized.')
+            }
+    except Exception as e:
+        print(f"Error processing image: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error processing image.')
+        }
